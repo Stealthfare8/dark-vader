5c2b3165ca57c097cb6cf334659ff3fe37c3cd7f
Stealthfare8 <160780051+Stealthfare8@users.noreply.github.com>
2024-06-09 12:28:41 +0100

    i want to sleep

--- index.py
+++ index.py
@@ -1,2 +1,12 @@
 print('I am Sleepy')
 print('Mbappe TO MADRID HERE WE GO')
+
+def grams_to_pounds(grams):
+    pounds = grams * 0.00220462
+    return pounds
+
+# User input
+grams = float(input("Enter the weight in grams: "))
+pounds = grams_to_pounds(grams)
+print(f"{grams} grams is equal to {pounds:.4f} pounds.")
+
